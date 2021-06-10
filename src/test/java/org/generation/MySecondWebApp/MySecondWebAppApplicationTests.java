2f911e33042420f6e88d7af787b459f6d9043987
akili <amwawe@gmail.com>
2021-06-10 12:34:51 -0500

    First commit

---
+++ src/test/java/org/generation/MySecondWebApp/MySecondWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.MySecondWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecondWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
